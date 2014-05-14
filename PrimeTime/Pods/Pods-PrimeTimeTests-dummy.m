8e4b2aaf1a9214fb77835149e09f46d5752a1ad0
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-05-14 14:30:31 -0400

    set up project

---
+++ PrimeTime/Pods/Pods-PrimeTimeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PrimeTimeTests : NSObject
+@end
+@implementation PodsDummy_Pods_PrimeTimeTests
+@end
---
+++ PrimeTime/PrimeTime/FISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FISAppDelegate.h
+//  PrimeTime
+//
+//  Created by Chris Gonzales on 5/14/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
