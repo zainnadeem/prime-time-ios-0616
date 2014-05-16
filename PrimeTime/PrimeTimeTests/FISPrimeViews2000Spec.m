8021acf00a42ccb9bab59f2ef19229dcec156f75
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-05-16 14:34:55 -0400

    add additional test files for 2000 and 5000 steps

---
+++ PrimeTime/PrimeTimeTests/FISPrimeViews2000Spec.m
@@ -0,0 +1,38 @@
+//
+//  FISPrimeViews2000Spec.m
+//  PrimeTime
+//
+//  Created by Chris Gonzales on 5/16/14.
+//  Copyright 2014 FIS. All rights reserved.
+//
+
+#import "Specta.h"
+#import "FISPrimeViews2000.h"
+
+
+SpecBegin(FISPrimeViews2000)
+
+describe(@"FISPrimeViews2000", ^{
+    
+    beforeAll(^{
+
+    });
+    
+    beforeEach(^{
+
+    });
+    
+    it(@"", ^{
+
+    });  
+    
+    afterEach(^{
+
+    });
+    
+    afterAll(^{
+
+    });
+});
+
+SpecEnd
---
+++ PrimeTime/PrimeTimeTests/FISPrimeViews5000Spec.m
@@ -0,0 +1,38 @@
+//
+//  FISPrimeViews5000Spec.m
+//  PrimeTime
+//
+//  Created by Chris Gonzales on 5/16/14.
+//  Copyright 2014 FIS. All rights reserved.
+//
+
+#import "Specta.h"
+#import "FISPrimeViews5000.h"
+
+
+SpecBegin(FISPrimeViews5000)
+
+describe(@"FISPrimeViews5000", ^{
+    
+    beforeAll(^{
+
+    });
+    
+    beforeEach(^{
+
+    });
+    
+    it(@"", ^{
+
+    });  
+    
+    afterEach(^{
+
+    });
+    
+    afterAll(^{
+
+    });
+});
+
+SpecEnd
