995dfdf15e3a9e24e3c596879426c37d03a8dc55
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-06-23 12:21:53 -0400

    add missing specs for 2000 and 5000

--- PrimeTime/PrimeTimeTests/FISPrimeViews2000Spec.m
+++ PrimeTime/PrimeTimeTests/FISPrimeViews2000Spec.m
@@ -2,37 +2,59 @@
 //  FISPrimeViews2000Spec.m
 //  PrimeTime
 //
-//  Created by Chris Gonzales on 5/16/14.
+//  Created by Chris Gonzales on 5/15/14.
 //  Copyright 2014 FIS. All rights reserved.
 //
 
+#ifdef PRIME2000
 #import "Specta.h"
-#import "FISPrimeViews2000.h"
-
+#import "FISPrimeTimeTableViewController.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
+#import "KIF.h"
+#import "Swizzlean.h"
 
 SpecBegin(FISPrimeViews2000)
 
-describe(@"FISPrimeViews2000", ^{
+describe(@"FISPrimeViews", ^{
+    __block FISPrimeTimeTableViewController *ptVC;
     
     beforeAll(^{
-
+        [KIFUITestActor setDefaultTimeout:5];
+        ptVC = [[FISPrimeTimeTableViewController alloc] init];
     });
     
     beforeEach(^{
-
+        
     });
     
-    it(@"", ^{
+    describe(@"tableview", ^{
+        it(@"has 100 cells, with proper prime number textlabel on each cell",^{
+            NSIndexPath *ip1 = [NSIndexPath indexPathForRow:0 inSection:0];
+            UITableViewCell *cell1 = (UITableViewCell *)[tester waitForCellAtIndexPath:ip1 inTableViewWithAccessibilityIdentifier:@"table"];
+            expect(cell1.textLabel.text).to.equal(@"17393");
+            
+            NSIndexPath *ip2 = [NSIndexPath indexPathForRow:49 inSection:0];
+            UITableViewCell *cell2 = (UITableViewCell *)[tester waitForCellAtIndexPath:ip2 inTableViewWithAccessibilityIdentifier:@"table"];
+            expect(cell2.textLabel.text).to.equal(@"17891");
+            
+            NSIndexPath *ip3 = [NSIndexPath indexPathForRow:99 inSection:0];
+            UITableViewCell *cell3 = (UITableViewCell *)[tester waitForCellAtIndexPath:ip3 inTableViewWithAccessibilityIdentifier:@"table"];
+            expect(cell3.textLabel.text).to.equal(@"18313");
 
-    });  
+            
+            
+        });
+    });
     
     afterEach(^{
-
+        
     });
     
     afterAll(^{
-
+        
     });
 });
 
 SpecEnd
+#endif
--- PrimeTime/PrimeTimeTests/FISPrimeViews5000Spec.m
+++ PrimeTime/PrimeTimeTests/FISPrimeViews5000Spec.m
@@ -2,37 +2,57 @@
 //  FISPrimeViews5000Spec.m
 //  PrimeTime
 //
-//  Created by Chris Gonzales on 5/16/14.
+//  Created by Chris Gonzales on 5/15/14.
 //  Copyright 2014 FIS. All rights reserved.
 //
 
+#ifdef PRIME5000
 #import "Specta.h"
-#import "FISPrimeViews5000.h"
-
+#import "FISPrimeTimeTableViewController.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
+#import "KIF.h"
+#import "Swizzlean.h"
 
 SpecBegin(FISPrimeViews5000)
 
-describe(@"FISPrimeViews5000", ^{
+describe(@"FISPrimeViews", ^{
+    __block FISPrimeTimeTableViewController *ptVC;
     
     beforeAll(^{
-
+        [KIFUITestActor setDefaultTimeout:5];
+        ptVC = [[FISPrimeTimeTableViewController alloc] init];
     });
     
     beforeEach(^{
-
+        
     });
     
-    it(@"", ^{
-
-    });  
+    describe(@"tableview", ^{
+        it(@"has 100 cells, with proper prime number textlabel on each cell",^{
+            NSIndexPath *ip1 = [NSIndexPath indexPathForRow:0 inSection:0];
+            UITableViewCell *cell1 = (UITableViewCell *)[tester waitForCellAtIndexPath:ip1 inTableViewWithAccessibilityIdentifier:@"table"];
+            expect(cell1.textLabel.text).to.equal(@"48619");
+            
+            NSIndexPath *ip2 = [NSIndexPath indexPathForRow:49 inSection:0];
+            UITableViewCell *cell2 = (UITableViewCell *)[tester waitForCellAtIndexPath:ip2 inTableViewWithAccessibilityIdentifier:@"table"];
+            expect(cell2.textLabel.text).to.equal(@"49123");
+            
+            NSIndexPath *ip3 = [NSIndexPath indexPathForRow:99 inSection:0];
+            UITableViewCell *cell3 = (UITableViewCell *)[tester waitForCellAtIndexPath:ip3 inTableViewWithAccessibilityIdentifier:@"table"];
+            expect(cell3.textLabel.text).to.equal(@"49663");
+            
+        });
+    });
     
     afterEach(^{
-
+        
     });
     
     afterAll(^{
-
+        
     });
 });
 
 SpecEnd
+#endif
